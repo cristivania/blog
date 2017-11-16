eed332feb809eb66ced9e40fa02479d5ba2f9b85
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-16 13:12:35 -0500

    Modal Box for projects(more info)

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,9 @@
 // about supported directives.
 //
 
+//= require jquery
 //= require rails-ujs
 //= require turbolinks
-//= require jquery
 //= require_tree .
 
 //Smooth Scrolling
@@ -47,33 +47,7 @@ $(document).ready(function() {
 
 });
 
-//popup button
-var modal = document.getElementById('simpleModal');
-// Get open modal button
-var modalBtn = document.getElementById('modalBtn');
-// Get close button
-var closeBtn = document.getElementsByClassName('closeBtn')[0];
-
-// Listen for open click
-modalBtn.addEventListener('click', openModal);
-// Listen for close click
-closeBtn.addEventListener('click', closeModal);
-// Listen for outside click
-window.addEventListener('click', outsideClick);
-
-// Function to open modal
-function openModal(){
-  modal.style.display = 'block';
-}
-
-// Function to close modal
-function closeModal(){
-  modal.style.display = 'none';
-}
-
-// Function to close modal if outside click
-function outsideClick(e){
-  if(e.target == modal){
-    modal.style.display = 'none';
-  }
-}
+$("#fade").modal({
+    fadeDuration: 1000,
+    fadeDelay: 0.50
+});
