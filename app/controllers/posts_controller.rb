2491bf63065ae19d8f3a07e3232dc3583dff108e
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-15 21:15:54 -0500

    Show page works/CSS back

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -10,11 +10,13 @@ class PostsController < ApplicationController
   # GET /posts/1
   # GET /posts/1.json
   def show
+    @post = Post.find(params[:id])
   end
 
   # GET /posts/new
   def new
     @post = Post.new
+
   end
 
   # GET /posts/1/edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   resources :posts
+
     get 'welcome' => 'welcome#index'
     root 'welcome#index'
-    get 'posts/:id', to: 'posts#show'
+    get 'post/:id', to: 'posts#show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
