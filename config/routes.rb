62c2327afb6fa393b1da3bfed6ea85cac41e6b62
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-21 19:43:53 -0500

    Routes for contacts

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :posts
+  resources :contacts, only: [:new, :create]
 
     get 'welcome' => 'welcome#index'
     root 'welcome#index'
 
-  
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
