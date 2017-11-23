32e40ca7805f8070acab60d263256e328452b027
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-22 22:43:42 -0500

    Fixing Layout

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :contacts, only: [:new, :create]
 
-    get 'welcome' => 'welcome#index'
-    root 'welcome#index'
+    
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
