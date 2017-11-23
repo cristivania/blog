ece4a04b422fa0cad5c331580914e5d965409967
Cristina Rosario <crosariobatres@yahoo.com>
2017-11-23 17:33:29 -0500

    Admin migration to User

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :posts
   resources :contacts, only: [:new, :create]
 
-  get 'welcome/index'
+  root 'posts#index'
 
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
