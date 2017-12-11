9bf7d6d43d0dd96be2260d33affc6c1d0ca71366
Cristina Rosario <crosariobatres@yahoo.com>
2017-12-11 15:03:23 -0500

    jquery turbolinks

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,12 +14,13 @@
 
 
 //= require jquery
+//= require jquery_ujs
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
 
 
-
+$(document).on('turbolinks:load', function() {
 
 //Smooth Scrolling
 $(document).ready(function() {
@@ -52,6 +53,29 @@ $(document).ready(function() {
 
 });
 
+$(function() {
+    //----- OPEN
+    $('[data-popup-open]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-open');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+        e.preventDefault();
+    });
+
+    //----- CLOSE
+    $('[data-popup-close]').on('click', function(e)  {
+        var targeted_popup_class = jQuery(this).attr('data-popup-close');
+        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+        e.preventDefault();
+    });
+  });
+});
+
+
+
+
+document.addEventListener("turbolinks:load", function() {
 
 var TxtRotate = function(el, toRotate, period) {
   this.toRotate = toRotate;
@@ -110,23 +134,4 @@ window.onload = function() {
   document.body.appendChild(css);
 };
 
-
-
-
-$(document).on('turbolinks:load', function() {
-    //----- OPEN
-    $('[data-popup-open]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-open');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
-
-        e.preventDefault();
-    });
-
-    //----- CLOSE
-    $('[data-popup-close]').on('click', function(e)  {
-        var targeted_popup_class = jQuery(this).attr('data-popup-close');
-        $('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
-
-        e.preventDefault();
-    });
 });
